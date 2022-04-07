99da2ed12cbf51373863a9fc5fb0a291e8d87afe
Frank Elavsky <frankelavsky@gmail.com>
2022-04-07 16:36:30 -0400

    add header, move h1 in, add skipnav

--- generate_html.js
+++ generate_html.js
@@ -3,19 +3,25 @@ const fs = require('fs');
 const converter = new showdown.Converter();
 
 const head = fs.readFileSync('includes/head.html', 'utf8');
+const skip = fs.readFileSync('includes/skip.html', 'utf8');
 const nav = fs.readFileSync('includes/nav.html', 'utf8');
 const footer = fs.readFileSync('includes/footer.html', 'utf8');
 const script = fs.readFileSync('includes/script.html', 'utf8');
-const readme = fs.readFileSync('workbook.md', 'utf8');
+let workbook = fs.readFileSync('workbook.md', 'utf8');
 
 const buildFile = () => {
-    // more to add here, style files, etc
+    // first, remove the h1 from this location (added in nav.html file)
+    const position = workbook.toString().indexOf('\n');
+    if (position != -1) {
+        workbook = workbook.substr(position + 1);
+    }
     let file = `<html lang="en-US">\n`
     file += head + `\n`
     file += `<body> \n`
+    file += skip + `\n`
     file += nav + `\n`
-    file += `<main> \n`
-    file += converter.makeHtml(readme)
+    file += `<main id="main"> \n`
+    file += converter.makeHtml(workbook)
     file += `</main> \n`
     file += footer + `\n`
     file += script + `\n`
