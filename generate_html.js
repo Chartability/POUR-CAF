5606e2b1fb2f969b03db020cd95543d92440ff36
Frank Elavsky <frankelavsky@gmail.com>
2022-04-07 16:21:27 -0400

    add footer, nav, and script sections

--- generate_html.js
+++ generate_html.js
@@ -3,13 +3,23 @@ const fs = require('fs');
 const converter = new showdown.Converter();
 
 const head = fs.readFileSync('includes/head.html', 'utf8');
+const nav = fs.readFileSync('includes/nav.html', 'utf8');
+const footer = fs.readFileSync('includes/footer.html', 'utf8');
+const script = fs.readFileSync('includes/script.html', 'utf8');
 const readme = fs.readFileSync('workbook.md', 'utf8');
+
 const buildFile = () => {
     // more to add here, style files, etc
     let file = `<html lang="en-US">\n`
     file += head + `\n`
+    file += `<body> \n`
+    file += nav + `\n`
+    file += `<main> \n`
     file += converter.makeHtml(readme)
-    file += `</html>`
+    file += `</main> \n`
+    file += footer + `\n`
+    file += script + `\n`
+    file += `</body></html>`
     return file
 }
 
