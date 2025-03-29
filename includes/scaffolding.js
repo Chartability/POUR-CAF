745a95043590c51433e20a43fe7be40ccf4e754d
Frank Elavsky <frankelavsky@gmail.com>
2025-03-29 11:51:24 -0400

    improve definition of DX

--- includes/scaffolding.js
+++ includes/scaffolding.js
@@ -67,7 +67,7 @@ module.exports = {
             p: [
                 "This workbook is intended to serve as a lightweight, high-level audit for catching data experience* design failures related to accessibility. For more on Chartability, see our home page: [https://chartability.fizz.studio/](https://chartability.fizz.studio/)",
                 `This workbook may be <a href="https://chartability.github.io/POUR-CAF/Chartability_Worksheet_V2.docx" download>downloaded as a standalone Microsoft Word document</a>, if you would like an editable file format (this is useful when keeping a record of evaluations).`,
-                `*A data experience (DX) could refer to a data visualization such as a chart, graph, or plot, a “bespoke” (highly customized) graphic based on data, a model, or an algorithm, or a data driven interface or system.`
+                `*A data experience (DX) could refer to a data visualization such as a chart, graph, or plot, a “bespoke” (highly customized) graphic based on data, a model, or an algorithm, or a data driven interface or system. If a human has an experience with data using any of their senses, then it counts as a data experience. We intentionally use an inclusive term.`
             ]
         },
         { 
